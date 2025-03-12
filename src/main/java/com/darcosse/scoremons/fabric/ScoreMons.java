2631feae906cf701735f766d48026b26129d2465
Bilel <bsahraoui.mail@gmail.com>
2025-03-12 13:28:09 +0100

    Added : Pokedex Caught Count objective

--- src/main/java/com/darcosse/scoremons/fabric/ScoreMons.java
+++ src/main/java/com/darcosse/scoremons/fabric/ScoreMons.java
@@ -2,8 +2,8 @@ package com.darcosse.scoremons.fabric;
 
 import com.cobblemon.mod.common.api.Priority;
 import com.cobblemon.mod.common.api.events.CobblemonEvents;
-import net.fabricmc.api.ModInitializer;
 import com.darcosse.scoremons.fabric.stats.ScoreboardStats;
+import net.fabricmc.api.ModInitializer;
 
 
 public class ScoreMons implements ModInitializer {
@@ -16,5 +16,6 @@ public class ScoreMons implements ModInitializer {
         CobblemonEvents.POKEMON_CAPTURED.subscribe(Priority.HIGHEST, ScoreboardStats.registerCapturedShinyPokemon());
         CobblemonEvents.BATTLE_VICTORY.subscribe(Priority.HIGHEST, ScoreboardStats.battleVictory());
         CobblemonEvents.FOSSIL_REVIVED.subscribe(Priority.HIGHEST, ScoreboardStats.revivedFossil());
+        //CobblemonEvents.POKEDEX_DATA_CHANGED_POST.subscribe(Priority.HIGHEST, ScoreboardStats.registerCaughtCount());
     }
 }
--- src/main/java/com/darcosse/scoremons/fabric/stats/ScoreboardStats.java
+++ src/main/java/com/darcosse/scoremons/fabric/stats/ScoreboardStats.java
@@ -1,18 +1,32 @@
 package com.darcosse.scoremons.fabric.stats;
 
+import com.cobblemon.mod.common.Cobblemon;
 import com.cobblemon.mod.common.api.events.battles.BattleVictoryEvent;
 import com.cobblemon.mod.common.api.events.pokemon.FossilRevivedEvent;
+import com.cobblemon.mod.common.api.events.pokemon.PokedexDataChangedEvent;
 import com.cobblemon.mod.common.api.events.pokemon.PokemonCapturedEvent;
+import com.cobblemon.mod.common.api.pokedex.CaughtCount;
+import com.cobblemon.mod.common.api.pokedex.PokedexManager;
+import com.cobblemon.mod.common.api.pokedex.PokedexValueCalculator;
+import com.cobblemon.mod.common.api.pokedex.entry.PokedexEntry;
+import com.cobblemon.mod.common.api.storage.player.client.ClientPokedexManager;
+import com.cobblemon.mod.common.client.CobblemonClient;
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.stats.Stat;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.scores.ScoreHolder;
 
 import java.util.UUID;
 
@@ -42,6 +56,16 @@ public class ScoreboardStats {
 
             if (player instanceof ServerPlayer serverPlayer) {
                 player.awardStat(Stats.CUSTOM.get(POKEMON_CAUGHT));
+
+                int count = Cobblemon.playerDataManager.getPokedexData(player.getUUID()).getDexCalculatedValue(
+                        ResourceLocation.tryParse("cobblemon:national"), CaughtCount.INSTANCE
+                );
+
+                player.resetStat(Stats.CUSTOM.get(POKEMON_REGISTERED));
+
+                for (int i = 0; i < count; i++) {
+                    player.awardStat(Stats.CUSTOM.get(POKEMON_REGISTERED));
+                }
 
                 if(event.getPokemon().isLegendary() || event.getPokemon().isMythical() || event.getPokemon().isUltraBeast()) {
                     for (Player p : player.getServer().getPlayerList().getPlayers()) {
@@ -116,6 +140,25 @@ public class ScoreboardStats {
                         );
                     }
                 }
+            }
+
+            return Unit.INSTANCE;
+        };
+    }
+
+    public static Function1<? super PokedexDataChangedEvent, Unit> registerCaughtCount() {
+        return event -> {
+
+            int count = event.getPokedexManager().getDexCalculatedValue(
+                    ResourceLocation.tryParse("cobblemon:national"), CaughtCount.INSTANCE
+            );
+
+            UUID playerUUID = event.getPlayerUUID();
+
+            Player player = Minecraft.getInstance().player.getServer().getPlayerList().getPlayer(playerUUID);
+
+            if (player instanceof ServerPlayer serverPlayer) {
+                serverPlayer.getStats().setValue(serverPlayer, Stats.CUSTOM.get(POKEMON_REGISTERED), count);
             }
 
             return Unit.INSTANCE;
